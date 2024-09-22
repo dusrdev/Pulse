cdde8f5705ab864435bcf99b9f5de8fa29dc57d7
David Shnayder <dusrdev@gmail.com>
2024-09-22 16:50:35 +0300

    Updated model

--- src/Pulse/Core/RequestDetails.cs
+++ src/Pulse/Core/RequestDetails.cs
@@ -1,12 +1,89 @@
 namespace Pulse.Core;
 
+/// <summary>
+/// Request details
+/// </summary>
 public class RequestDetails {
+	/// <summary>
+	/// Default request details
+	/// </summary>
 	public static readonly RequestDetails Default = new();
 
-	public bool BypassProxy { get; set; } = true;
-	public string? ProxyHost { get; set; }
-	public string? ProxyUsername { get; set; }
-	public string? ProxyPassword { get; set; }
+	/// <summary>
+	/// Proxy configuration
+	/// </summary>
+	public Proxy Proxy { get; set; } = new();
 
-	public HttpRequestMessage? RequestMessage { get; set; } = new(HttpMethod.Get, "https://ipinfo.io/geo");
+	/// <summary>
+	/// Request configuration
+	/// </summary>
+	public Request Request { get; set; } = new();
+}
+
+/// <summary>
+/// Proxy configuration
+/// </summary>
+public class Proxy {
+	/// <summary>
+	/// Don't use proxy
+	/// </summary>
+	public bool Bypass { get; set; } = true;
+
+	/// <summary>
+	/// Host
+	/// </summary>
+	public string? Host { get; set; }
+
+	/// <summary>
+	/// Proxy authentication username
+	/// </summary>
+	public string? Username { get; set; }
+
+	/// <summary>
+	/// Proxy authentication password
+	/// </summary>
+	public string? Password { get; set; }
+}
+
+/// <summary>
+/// Request configuration
+/// </summary>
+public class Request {
+	/// <summary>
+	/// Request URL - defaults to https://ipinfo.io/geo
+	/// </summary>
+	public string Url { get; set; } = "https://ipinfo.io/geo";
+
+	/// <summary>
+	/// Request method - defaults to GET
+	/// </summary>
+	public HttpMethod Method { get; set; } = HttpMethod.Get;
+
+	/// <summary>
+	/// Request headers
+	/// </summary>
+	public Dictionary<string, string?> Headers { get; set; } = [];
+
+	/// <summary>
+	/// Request body
+	/// </summary>
+	public string? Body { get; set; }
+
+	/// <summary>
+	/// Create an http request message from the configuration
+	/// </summary>
+	/// <returns><see cref="HttpRequestMessage"/></returns>
+	public HttpRequestMessage CreateMessage() {
+		var message = new HttpRequestMessage(Method, Url);
+
+		foreach (var header in Headers) {
+			message.Headers.Add(header.Key, header.Value);
+		}
+
+		if (Body is not null) {
+			message.Content = new StringContent(Body);
+		}
+
+		return message;
+	}
 }
