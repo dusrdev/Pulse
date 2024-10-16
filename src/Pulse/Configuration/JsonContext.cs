1fdba7b4df8a6a536f63c3ec5ede4b8ae1aa1fad
David Shnayder <dusrdev@gmail.com>
2024-10-16 18:22:47 +0300

    Added simpler function to load request details

--- src/Pulse/Configuration/JsonContext.cs
+++ src/Pulse/Configuration/JsonContext.cs
@@ -3,11 +3,13 @@ using System.Text.Json.Serialization;
 
 using Pulse.Core;
 
+using Sharpify;
+
 namespace Pulse.Configuration;
 
 [JsonSourceGenerationOptions(AllowTrailingCommas = true,
 							 DefaultIgnoreCondition = JsonIgnoreCondition.Never,
-							 ReadCommentHandling = System.Text.Json.JsonCommentHandling.Skip,
+							 ReadCommentHandling = JsonCommentHandling.Skip,
 							 UnknownTypeHandling = JsonUnknownTypeHandling.JsonNode,
 							 PropertyNameCaseInsensitive = true,
 							 UnmappedMemberHandling = JsonUnmappedMemberHandling.Skip,
@@ -19,4 +21,27 @@ namespace Pulse.Configuration;
 [JsonSerializable(typeof(StrippedException))]
 [JsonSerializable(typeof(JsonElement))]
 public partial class JsonContext : JsonSerializerContext {
+	/// <summary>
+	/// Try to get request details from file
+	/// </summary>
+	/// <param name="path"></param>
+	/// <returns></returns>
+	public static Result<RequestDetails> TryGetRequestDetailsFromFile(string path) {
+		try {
+			if (!File.Exists(path)) {
+				return Result.Fail<RequestDetails>($"{path} - does not exist.", new());
+			}
+
+			using var file = File.OpenRead(path);
+			var rd = JsonSerializer.Deserialize(file, Default.RequestDetails);
+			if (rd is null) {
+				return Result.Fail<RequestDetails>($"{path} - contained empty or invalid JSON.", new());
+			}
+			return Result.Ok(rd);
+		} catch (Exception e) {
+			var stripped = new StrippedException(e);
+			var message = JsonSerializer.Serialize(stripped, Default.StrippedException);
+			return Result.Fail<RequestDetails>(message, new());
+		}
+	}
 }
