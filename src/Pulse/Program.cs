faa9c86ef18e058ba4a50f87539545f80a047b5d
David Shnayder <dusrdev@gmail.com>
2024-11-06 19:59:02 +0200

    Updated version

--- src/Pulse/Program.cs
+++ src/Pulse/Program.cs
@@ -7,7 +7,7 @@ using static PrettyConsole.Console;
 using PrettyConsole;
 
 internal class Program {
-	internal const string VERSION = "1.0.2";
+	internal const string VERSION = "1.0.2.0";
 
     private static async Task<int> Main(string[] args) {
         using CancellationTokenSource globalCTS = new();
