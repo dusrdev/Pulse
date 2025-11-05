df364efd54c007aac364369d7168224ad18d6956
David Shnayder <dusrdev@gmail.com>
2025-11-05 19:58:44 +0200

    Preserve command method info to workaround trimming

--- src/Pulse/Program.cs
+++ src/Pulse/Program.cs
@@ -1,17 +1,24 @@
-﻿using ConsoleAppFramework;
+﻿using System.Diagnostics.CodeAnalysis;
+
+using ConsoleAppFramework;
 
 using Pulse.Core;
 
-ConsoleApp.Version = Commands.VERSION;
+internal partial class Program {
+	[DynamicDependency(DynamicallyAccessedMemberTypes.PublicMethods, typeof(Commands))]
+    private static async Task Main(string[] args) {
+        ConsoleApp.Version = Commands.VERSION;
 
-var app = ConsoleApp.Create();
+        var app = ConsoleApp.Create();
 
-app.UseFilter<GlobalExceptionHandler>();
+        app.UseFilter<GlobalExceptionHandler>();
 
-app.Add("", Commands.Root);
-app.Add("get-sample", Commands.GetSample);
-app.Add("get-schema", Commands.GetSchema);
-app.Add("check-for-updates", Commands.CheckForUpdates);
-app.Add("terms-of-use", Commands.TermsOfUse);
+        app.Add("", Commands.Root);
+        app.Add("get-sample", Commands.GetSample);
+        app.Add("get-schema", Commands.GetSchema);
+        app.Add("check-for-updates", Commands.CheckForUpdates);
+        app.Add("terms-of-use", Commands.TermsOfUse);
 
-await app.RunAsync(args);
+        await app.RunAsync(args);
+    }
+}
