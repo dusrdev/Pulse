c7e2d6d2026395af09740de1a3c245e47d2bc58f
David Shnayder <dusrdev@gmail.com>
2025-11-15 16:17:08 +0200

    Add file-based app for profiling

---
+++ profiling/runner.cs
@@ -0,0 +1,40 @@
+#:package PrettyConsole@5.0.0
+#:package CliWrap@3.9.0
+
+using CliWrap;
+using CliWrap.Buffered;
+
+using PrettyConsole;
+
+string directory = "";
+string executable = "";
+
+string[][] commands = [
+	["--help"], // show help
+	["get-sample"], // get-sample json file
+	["sample.json", "-u", "http://127.0.0.1:3000/"], // single request - default
+	["sample.json", "-u", "http://127.0.0.1:3000/json/", "-n", "100", "--json"], // 100 concurrent + format
+	["sample.json", "-u", "http://127.0.0.1:3000/html/", "-n", "100", "-c", "10", "--raw"], // no format
+	["sample.json", "-u", "http://127.0.0.1:3000/html/", "-n", "10", "-f", "--raw"], // full equality
+	["sample.json", "-u", "http://127.0.0.1:3000/html/", "-n", "100", "-v", "--raw"], // verbose mode
+];
+
+foreach (var command in commands) {
+	var result = await Cli.Wrap(executable)
+					.WithArguments(command)
+					.WithWorkingDirectory(directory)
+					.ExecuteBufferedAsync();
+	if (result.ExitCode != 0) {
+		if (result.StandardOutput.Length > 0) {
+			Console.WriteLineInterpolated($"{ConsoleColor.Green}Standard output:");
+			Console.Write(result.StandardOutput.AsSpan(), OutputPipe.Out);
+		}
+		if (result.StandardError.Length > 0) {
+			Console.WriteLineInterpolated($"{ConsoleColor.Green}Standard error:");
+			Console.Write(result.StandardError.AsSpan(), OutputPipe.Out);
+		}
+		return 1;
+	}
+}
+
+return 0;
