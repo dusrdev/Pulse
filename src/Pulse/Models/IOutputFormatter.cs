52e079a3ac66cad4e1daaf25e522c30fcb5eedbe
David Shnayder <dusrdev@gmail.com>
2025-11-22 13:09:35 +0200

    Removed unnecessary generic

--- src/Pulse/Models/IOutputFormatter.cs
+++ src/Pulse/Models/IOutputFormatter.cs
@@ -1,6 +1,6 @@
 namespace Pulse.Models;
 
-internal interface IOutputFormatter<T> where T : allows ref struct {
+internal interface IOutputFormatter {
 	abstract void OutputAsPlainText();
 
 	abstract void OutputAsJson();
@@ -12,7 +12,7 @@ internal enum OutputFormat {
 }
 
 internal static class OutputFormatterExtensions {
-	internal static void Output<T>(this T value, OutputFormat format) where T : IOutputFormatter<T>, allows ref struct {
+	internal static void Output<T>(this T value, OutputFormat format) where T : IOutputFormatter, allows ref struct {
 		switch (format) {
 			case OutputFormat.PlainText:
 				value.OutputAsPlainText();
