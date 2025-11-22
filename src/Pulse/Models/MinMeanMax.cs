a8c29aff56bf7434c4127f3467e402b1ca8a2348
David Shnayder <dusrdev@gmail.com>
2025-11-22 13:09:44 +0200

    Added summary models

---
+++ src/Pulse/Models/MinMeanMax.cs
@@ -0,0 +1,7 @@
+namespace Pulse.Models;
+
+internal readonly struct MinMeanMax {
+	public required double Min { get; init; }
+	public required double Mean { get; init; }
+	public required double Max { get; init; }
+}
---
+++ src/Pulse/Models/SummaryModel.cs
@@ -0,0 +1,45 @@
+using System.Net;
+using System.Text.Json;
+
+using Pulse.Core;
+
+namespace Pulse.Models;
+
+internal readonly struct SummaryModel : IOutputFormatter {
+	public required int RequestCount { get; init; }
+	public required int ConcurrentConnections { get; init; }
+	public required TimeSpan TotalDuration { get; init; }
+	public required double SuccessRate { get; init; }
+	public required MinMeanMax Latency { get; init; }
+	public required int LatencyOutliersRemoved { get; init; }
+	public required MinMeanMax ContentSize { get; init; }
+	public required double ThroughputBytesPerSecond { get; init; }
+	public required Dictionary<HttpStatusCode, int> StatusCodeCounts { get; init; }
+
+	public void OutputAsJson() {
+		JsonSerializer.ToConsoleOut(in this, ModelsJsonContext.Default.SummaryModel);
+	}
+
+	public void OutputAsPlainText() {
+		Console.WriteLineInterpolated($"Request count: {Yellow}{RequestCount}");
+		Console.WriteLineInterpolated($"Concurrent connections: {Yellow}{ConcurrentConnections}");
+		Console.WriteLineInterpolated($"Total duration: {Yellow}{TotalDuration:duration}");
+		Console.WriteLineInterpolated($"Success Rate: {Helper.GetPercentageBasedColor(SuccessRate)}{SuccessRate}%");
+		Console.WriteLineInterpolated($"Latency:       Min: {Green}{Latency.Min:0.##}ms{ConsoleColor.Default}, Mean: {Yellow}{Latency.Mean:0.##}ms{ConsoleColor.Default}, Max: {Red}{Latency.Max:0.##}ms");
+		if (LatencyOutliersRemoved != 0) {
+			Console.WriteLineInterpolated($"               (Removed {DarkYellow}{LatencyOutliersRemoved}{ConsoleColor.Default} {(LatencyOutliersRemoved == 1 ? "outlier" : "outliers")})");
+		}
+		Console.WriteLineInterpolated($"Content Size:  Min: {Green}{ContentSize.Min:bytes}{ConsoleColor.Default}, Mean: {Yellow}{ContentSize.Mean:bytes}{ConsoleColor.Default}, Max: {Red}{ContentSize.Max:bytes}");
+		Console.WriteLineInterpolated($"Total throughput: {Yellow}{ThroughputBytesPerSecond:bytes}/s");
+		Console.WriteLineInterpolated($"Status codes:");
+		foreach (var kvp in StatusCodeCounts.OrderBy(static s => (int)s.Key)) {
+			var key = (int)kvp.Key;
+			if (key is 0) {
+				Console.WriteLineInterpolated($"   {Magenta}{key}{ConsoleColor.Default} --> {kvp.Value}  [StatusCode 0 = Exception]");
+			} else {
+				Console.WriteLineInterpolated($"   {Helper.GetStatusCodeBasedColor(key)}{key}{ConsoleColor.Default} --> {kvp.Value}");
+			}
+		}
+		Console.NewLine();
+	}
+}
