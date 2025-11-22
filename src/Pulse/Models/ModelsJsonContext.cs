174b69abbe258c6245922d944b1ff23b23248431
David Shnayder <dusrdev@gmail.com>
2025-11-22 13:10:01 +0200

    Use one serializer context for models

---
+++ src/Pulse/Models/ModelsJsonContext.cs
@@ -0,0 +1,21 @@
+using System.Collections.Immutable;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+
+namespace Pulse.Models;
+
+[JsonSerializable(typeof(SummaryModel))]
+[JsonSerializable(typeof(MinMeanMax))]
+[JsonSerializable(typeof(ImmutableArray<string>))]
+internal partial class ModelsJsonContext : JsonSerializerContext;
+
+internal static class JsonSerializerExtensions {
+	extension(JsonSerializer) {
+		internal static void ToConsoleOut<T>(in T value, JsonTypeInfo<T> jsonTypeInfo) {
+			using var writer = new Utf8JsonWriter(Console.OpenStandardOutput());
+			JsonSerializer.Serialize(writer, value, jsonTypeInfo);
+			writer.Flush();
+		}
+	}
+}
